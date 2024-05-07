21c4f2b162290358a1a390add8fe94f40ce12a6a
park20011029 <rokmck156@gmail.com>
2024-05-07 23:28:49 +0900

    Feat: Add awss3 config and util

---
+++ secureProject/src/main/java/secure/project/secureProject/configuration/S3Config.java
@@ -0,0 +1,30 @@
+package secure.project.secureProject.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class S3Config {
+
+        @Value("${cloud.aws.credentials.access-key}")
+        private String iamAccessKey;
+        @Value("${cloud.aws.credentials.secret-key}")
+        private String iamSecretKey;
+        @Value("${cloud.aws.region.static}")
+        private String region;
+
+        @Bean
+        public AmazonS3Client  amazonS3Client(){
+                BasicAWSCredentials awsCredentials = new BasicAWSCredentials(iamAccessKey, iamSecretKey);
+                return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                        .withRegion(region).enablePathStyleAccess()
+                        .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                        .build();
+        }
+}
