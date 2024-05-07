bd399366ee8a3454205570c1222996f942c62c9d
park20011029 <rokmck156@gmail.com>
2024-05-07 23:29:11 +0900

    Feat: Aadd customer payment

---
+++ secureProject/src/main/java/secure/project/secureProject/dto/reqeust/CustomerOrderItemRequestDto.java
@@ -0,0 +1,14 @@
+package secure.project.secureProject.dto.reqeust;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CustomerOrderItemRequestDto {
+    private Long userId;
+    private Long itemId;
+    private Integer buyItemAmount;
+}
