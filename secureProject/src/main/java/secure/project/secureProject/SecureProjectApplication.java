5c6cfdea6043376138e97c9b149607f25d3e1a9a
park20011029 <rokmck156@gmail.com>
2024-06-05 15:03:33 +0900

    Feat: Add aop for log

--- secureProject/src/main/java/secure/project/secureProject/SecureProjectApplication.java
+++ secureProject/src/main/java/secure/project/secureProject/SecureProjectApplication.java
@@ -3,10 +3,11 @@ package secure.project.secureProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableAspectJAutoProxy
 public class SecureProjectApplication {
-
     public static void main(String[] args) {
 
         SpringApplication.run(SecureProjectApplication.class, args);
